1b93e620b1d1950fd7e585c362c2f59e38451b3f
Joffre M. <Joffre.Molina@gmail.com>
2025-03-24 18:56:03 -0400

    Solution of prob 1

--- Euler_prob_1.py
+++ Euler_prob_1.py
@@ -4,3 +4,15 @@
 #
 # Find the sum of all the multiples of 3 or 5 below 1000
 
+import math
+
+sum = 0
+limit = 1000
+
+for x in range(1,limit):
+    if x % 3 == 0 or x % 5 == 0:
+        sum += x
+    
+print("The sum is {}".format(sum))
+
+    
